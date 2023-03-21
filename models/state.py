595212ea07c71425568c2150ba429a970f661a3d
Joseph Kakai <josephngalu96@gmail.com>
2023-03-21 10:29:44 -0700

    created models/engine/db_storage.py and updated models/base_model.py, models/city.py, models/state.py, models/__init__.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("city", cascade="all, delete-orphan", backref="state")
+
+    @property
+    def cities(self):
+        """getter attribute that returns the list of city instances with state_id equals to the current State.id"""
+        city_list = []
+        for city in models.storage.all("city").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
