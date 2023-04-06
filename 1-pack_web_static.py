a9b8d197b9ae128d08deae718ecc26314d7bac71
Wanton Raven <josephngalu96@gmail.com>
2023-04-06 12:41:57 +0000

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""a Fabric script that generates a .tgz archive\
-        from the contents of the web_static folder\
-        of your AirBnB Clone repo, using the\
-        function do_pack."""
+"""a fabric script to create an archive file"""
 from fabric.api import local
 from datetime import datetime
-import os
-
 
 def do_pack():
-    """create .tgz from web_static"""
-    """creates archive with current date and time"""
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_{}.tgz".format(now)
-
-    """compiress contents of web_static into archive"""
+    """ a method to compress a file and return it's path """
+    
+    """saving the current time"""
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(time_now)
+   
     try:
-        if not os.path.exists("versions"):
-            local("mkdir -p versions")
-        local("tar -cvzf versions/{} w`eb_static".format(archive_name))
-        return "versions/{}".format(archive_name)
+        """create a directory"""
+        local("mkdir -p versions")
+         
+        """create an archive file"""
+        local("tar -cvzf {} web_static/".format(file_path))
+        
+        """return the path to the archive file created"""
+        return "{}".format(file_path)
+
     except Exception as e:
+        print("An error occurred: {}".format(str(e)))
         return None
+
