cb165483da70f9c4286d6661d84a1a934fd7659c
root <Wanton Raven>
2023-04-06 03:08:32 -0700

    updated code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-i#!/usr/bin/env bash
+#!/usr/bin/env bash
 #  a Bash script that sets up your web servers for the deployment of web_static
 # install nginx if not installed
 if [ "$(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c 'installed')" -eq 0 ];
@@ -37,21 +37,28 @@ if [ ! -d '/data/web_static/releases/test/' ]; then
 fi
 
 # Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-echo "<html><body><h1>Test Page</h1></body></html>" > /data/web_static/releases/test/index.html
+sudo tee /data/web_static/releases/test/index.html >/dev/null <<EOF
+<!DOCTYPE html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>
+EOF
 
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 if [ -L '/data/web_static/current' ]; then
         sudo rm /data/web_static/current
 fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static
 WEBSTATIC="/etc/nginx/sites-available/default"
-NGINX_CONF="/data/web_static/current/"
-sudo sed -i 's#root /var/www/html;#root /var/www/html;\n\tlocation /hbnb_static/ {\n\talias '"$WEBSTATIC"'\n\t}#' "$NGINX_CONF"
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo nginx -t
 
