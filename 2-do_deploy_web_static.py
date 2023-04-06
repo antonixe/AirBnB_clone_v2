10282f852651d73c0aee3742b918cdd6e89831dd
Joseph Kakai <josephngalu96@gmail.com>
2023-04-06 13:11:59 -0700

    updated task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,47 +5,34 @@ and decompress it"""
 from fabric.api import run, env, put
 import os.path
 
-env.hosts = ['100.26.154.245', '54.144.221.234']
-env.key_filename = '~/.ssh/my_ssh_key'
+env.hosts = ['54.162.93.251', '100.25.3.37']
+env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
 
-def deploy_code(archive_path):
+def do_deploy(archive_path):
     """a function to deploy code and decompress it"""
     
     # Check if the archive file exists and is not a directory
     if not os.path.isfile(archive_path) or os.path.isdir(archive_path):
         return False
     
-    compressed_file = archive_path.split("/")[-1]
-    no_extension = compressed_file.split(".")[0]
-    
     try:
-        remote_path = "/var/www/releases/{}/".format(no_extension)
-        sym_link = "/var/www/current"
+        remote_path = "/data/web_static/releases/"
+        sym_link = "/data/web_static/current"
         
         # Upload the archive file to the remote server
         put(archive_path, "/tmp/")
         
-        # Create the directory for the new release
-        run("sudo mkdir -p {}".format(remote_path))
-        
-        # Extract the files from the archive
-        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+        # Extract the files from the archive to a new release directory
+        run("sudo tar -xvzf /tmp/{} -C {} --strip-components=1".format(os.path.basename(archive_path), remote_path))
         
         # Remove the archive file from the remote server
-        run("sudo rm /tmp/{}".format(compressed_file))
-        
-        # Move the extracted files to the release directory
-        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
-        
-        # Remove the old web_static directory
-        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo rm /tmp/{}".format(os.path.basename(archive_path)))
         
         # Update the symbolic link to point to the new release
         run("sudo rm -rf {}".format(sym_link))
-        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        run("sudo ln -s {} {}".format(remote_path, sym_link))
         
         return True
     except Exception as e:
         return False
-
