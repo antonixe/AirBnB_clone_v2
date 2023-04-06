f6b8fd3a8ea8b574217e6c60d21b63d529ff0576
Joseph Kakai <josephngalu96@gmail.com>
2023-04-06 13:18:58 -0700

    updated task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,30 +9,32 @@ env.hosts = ['54.144.221.234', '100.26.154.245']
 env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """a function to deploy code and decompress it"""
-    
+
     # Check if the archive file exists and is not a directory
     if not os.path.isfile(archive_path) or os.path.isdir(archive_path):
         return False
-    
+
     try:
         remote_path = "/data/web_static/releases/"
         sym_link = "/data/web_static/current"
-        
+
         # Upload the archive file to the remote server
         put(archive_path, "/tmp/")
-        
+
         # Extract the files from the archive to a new release directory
-        run("sudo tar -xvzf /tmp/{} -C {} --strip-components=1".format(os.path.basename(archive_path), remote_path))
-        
+        run("sudo tar -xvzf /tmp/{} -C {} --strip-components=1".format(
+            os.path.basename(archive_path), remote_path))
+
         # Remove the archive file from the remote server
         run("sudo rm /tmp/{}".format(os.path.basename(archive_path)))
-        
+
         # Update the symbolic link to point to the new release
         run("sudo rm -rf {}".format(sym_link))
         run("sudo ln -s {} {}".format(remote_path, sym_link))
-        
+
         return True
     except Exception as e:
         return False
