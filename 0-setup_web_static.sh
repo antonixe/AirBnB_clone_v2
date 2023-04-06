5bb0385fc34b3994c029f14445f991fa4f66829b
Joseph Kakai <josephngalu96@gmail.com>
2023-04-06 10:27:50 -0700

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# A script that installs and configs nginx
-# The steps include
-# install nginx, creates the repositories, symbolic link also give ownership to /data/,configuring nginx and restart it
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test/ 
-sudo mkdir -p /data/web_static/shared/
-echo " Holberton School" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Install Nginx if not already installed
+command -v nginx > /dev/null || sudo apt-get -y update && sudo apt-get -y install nginx
+
+# Create necessary directories and files
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link and give ownership to ubuntu user and group
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+
+# Update Nginx configuration to serve content from /data/web_static/current/ to hbnb_static
+sudo sed -i '/^\tserver_name localhost;$/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
