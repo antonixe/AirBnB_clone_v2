4886d265a4f8002c125917f8e8d0ac15c205e4be
Wanton Raven <josephngalu96@gmail.com>
2023-04-06 12:09:50 +0000

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,24 +5,21 @@
         function do_pack."""
 from fabric.api import local
 from datetime import datetime
-from os import makedirs, path
+import os
 
 
 def do_pack():
     """create .tgz from web_static"""
-
-    # create directory
-    if not path.exists("versions"):
-        makedirs("versions")
-
-    # creates archive with current date and time
+    """creates archive with current date and time"""
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(now)
 
-    # compress contents of web_static into archive
+    """compress contents of web_static into archive"""
     try:
+   	if not os.path.exists("versions")
+		local("mkdir -p versions")
+
         local("tar -cvzf versions/{} web_static".format(archive_name))
         return "versions/{}".format(archive_name)
     except (subprocess.CalledProcessError, OSError) as e:
-        print(f"Error: {e}")
         return None
