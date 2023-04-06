91735de7966a3bc87e4058525ebdcf2e39706a69
Joseph Kakai <josephngalu96@gmail.com>
2023-04-06 12:07:41 -0700

    created task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script to deploy an archive file to a remote server and decompress it.
+"""
+import os.path
+from fabric.api import env, put, run
+
+env.hosts = ['54.162.93.251', '100.25.3.37']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
+
+
+def deploy_archive(archive_path):
+    """
+    Deploy an archive file to a remote server and decompress it.
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    filename = os.path.basename(archive_path)
+    filename_no_ext = os.path.splitext(filename)[0]
+    remote_dir = "/data/web_static/releases/{}/".format(filename_no_ext)
+    current_dir = "/data/web_static/current"
+
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(remote_dir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(filename, remote_dir))
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo mv {}web_static/* {}".format(remote_dir, remote_dir))
+        run("sudo rm -rf {}web_static/".format(remote_dir))
+        run("sudo rm -rf {}".format(current_dir))
+        run("sudo ln -s {} {}".format(remote_dir, current_dir))
+        return True
+    except Exception:
+        return False
