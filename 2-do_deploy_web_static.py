8f1deec9c9ee7a7d96d757c3573e18574730803e
Joseph Kakai <josephngalu96@gmail.com>
2023-04-06 13:07:22 -0700

    updated task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,51 @@
 #!/usr/bin/python3
-"""
-A script to deploy an archive file to a remote server and decompress it.
-"""
-import os.path
-from fabric.api import env, put, run
+"""a script to send an archive file to a remote server
+and decompress it"""
 
-env.hosts = ['54.144.221.234', '100.26.154.245']
-env.key_filename = '~/.ssh/school'
+from fabric.api import run, env, put
+import os.path
+
+env.hosts = ['100.26.154.245', '54.144.221.234']
+env.key_filename = '~/.ssh/my_ssh_key'
 env.user = 'ubuntu'
 
-
-def deploy_archive(archive_path):
-    """
-    Deploy an archive file to a remote server and decompress it.
-    """
-    if not os.path.isfile(archive_path):
+def deploy_code(archive_path):
+    """a function to deploy code and decompress it"""
+    
+    # Check if the archive file exists and is not a directory
+    if not os.path.isfile(archive_path) or os.path.isdir(archive_path):
         return False
-
-    filename = os.path.basename(archive_path)
-    filename_no_ext = os.path.splitext(filename)[0]
-    remote_dir = "/data/web_static/releases/{}/".format(filename_no_ext)
-    current_dir = "/data/web_static/current"
-
+    
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+    
     try:
+        remote_path = "/var/www/releases/{}/".format(no_extension)
+        sym_link = "/var/www/current"
+        
+        # Upload the archive file to the remote server
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(remote_dir))
-        run("sudo tar -xzf /tmp/{} -C {}".format(filename, remote_dir))
-        run("sudo rm /tmp/{}".format(filename))
-        run("sudo mv {}web_static/* {}".format(remote_dir, remote_dir))
-        run("sudo rm -rf {}web_static/".format(remote_dir))
-        run("sudo rm -rf {}".format(current_dir))
-        run("sudo ln -s {} {}".format(remote_dir, current_dir))
+        
+        # Create the directory for the new release
+        run("sudo mkdir -p {}".format(remote_path))
+        
+        # Extract the files from the archive
+        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+        
+        # Remove the archive file from the remote server
+        run("sudo rm /tmp/{}".format(compressed_file))
+        
+        # Move the extracted files to the release directory
+        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
+        
+        # Remove the old web_static directory
+        run("sudo rm -rf {}/web_static".format(remote_path))
+        
+        # Update the symbolic link to point to the new release
+        run("sudo rm -rf {}".format(sym_link))
+        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        
         return True
-    except Exception:
+    except Exception as e:
         return False
+
