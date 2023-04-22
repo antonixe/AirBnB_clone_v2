7265d784fd2b32ef7607e838302f22d51b469450
Joseph Kakai <josephngalu96@gmail.com>
2023-04-22 14:17:06 -0700

    corrected error

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def number(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
+def number_template(n=None):
     return render_template("5-number.html", n=n)
 
 
