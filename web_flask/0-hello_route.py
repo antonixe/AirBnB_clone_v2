dcd14807f9a67e2c2aa0d0644913e286cc7df447
Wanton Raven <josephngalu96@gmail.com>
2023-05-15 11:43:10 +0000

    updated hello route file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_HBNB():
     """a return hello HBNB method"""
     return "Hello HBNB!"
